d1e1704e7b8ebc903209b9fa3c0dc7e36d9efdea
apostolis <a.taxidaridis@gmail.com>
2014-09-07 18:40:19 +0300

    adding pins with scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140906145232) do
+ActiveRecord::Schema.define(version: 20140907105439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
