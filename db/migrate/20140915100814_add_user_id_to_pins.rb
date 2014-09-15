75b2d36a426f616e926fb0814b8ca52f0def6805
apostolis <a.taxidaridis@gmail.com>
2014-09-15 14:16:25 +0300

    Add user/pins association
    This involves authentication as well

---
+++ db/migrate/20140915100814_add_user_id_to_pins.rb
@@ -0,0 +1,5 @@
+class AddUserIdToPins < ActiveRecord::Migration
+  def change
+    add_column :pins, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140907105439) do
+ActiveRecord::Schema.define(version: 20140915100814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140907105439) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
